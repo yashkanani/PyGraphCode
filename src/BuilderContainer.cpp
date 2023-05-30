7bfba048afefd118fe66437bc6ee58e613ca2f4e
Yash <yash001@admiralinstruments.com>
2023-05-30 12:48:51 -0700

    apply default margin.

--- src/BuilderContainer.cpp
+++ src/BuilderContainer.cpp
@@ -15,7 +15,6 @@ BuilderContainer::BuilderContainer(QWidget* parent)
 
     setAcceptDrops(true);
     builderContainerlayout = new QVBoxLayout(this);
-    builderContainerlayout->setContentsMargins(0, 0, 0, 15);
     builderContainerlayout->addStretch(1);
     setLayout(builderContainerlayout);
 
