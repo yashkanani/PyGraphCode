fa44c5912d9fd4047058fb1fea29dc40870bb6a7
Yash <yash001@admiralinstruments.com>
2023-06-22 22:57:25 -0700

    protect from removing elements it self while internal drag of elements; if dragged element is parents of drop container.
    if allow to remove the elements then all elements will be remove all elements because parents is gone die.

--- src/InternalDragEventHandler.cpp
+++ src/InternalDragEventHandler.cpp
@@ -1,27 +1,27 @@
 #include "InternalDragEventHandler.h"
-#include "BuilderContainer.h" 
 #include "AbstractElement.h"
+#include "BuilderContainer.h"
 
+#include <qdrag.h>
 #include <qevent.h>
 #include <qlabel.h>
-#include <qdrag.h>
 #include <qmimedata.h>
 
 InternalDragEventHandler::InternalDragEventHandler(QObject* parent)
     : QObject(parent)
-    
+
 {
 }
 
 bool InternalDragEventHandler::eventFilter(QObject* watched, QEvent* event)
 {
-    QWidget* label = qobject_cast<QWidget*>(watched);
-    if (label && event->type() == QEvent::MouseButtonPress) {
-        if (label->property("element").isValid()) {
-            AbstractElement* element = qvariant_cast<AbstractElement*>(label->property("element"));
+    QWidget* draggedWidget = qobject_cast<QWidget*>(watched);
+    if (draggedWidget && event->type() == QEvent::MouseButtonPress) {
+        if (draggedWidget->property("element").isValid()) {
+            AbstractElement* element = qvariant_cast<AbstractElement*>(draggedWidget->property("element"));
 
             // Create a drag object
-            QDrag* drag = new QDrag(label);
+            QDrag* drag = new QDrag(draggedWidget);
             QMimeData* mimeData = new QMimeData;
 
             // Set the element as the mime data
@@ -35,16 +35,31 @@ bool InternalDragEventHandler::eventFilter(QObject* watched, QEvent* event)
 
             // Start the drag
             if (drag->exec(Qt::CopyAction | Qt::MoveAction) == Qt::MoveAction) {
-                // Remove the dragged element from containerInformation
-                BuilderContainer* builderContainer = dynamic_cast<BuilderContainer*>(parent());
-                if (builderContainer) {
-                    builderContainer->removeElementFromContainerInformation(element);
+               
+                QObject* target = drag->target();
+
+                if (target) {
+
+                    // If the dragged element is placed inside the child container of the dragged element,
+                    // we don't want to remove the dragged element. Otherwise, all elements, including the new element, will be removed.
+                    BuilderContainer* targetContainer = dynamic_cast<BuilderContainer*>(target); 
+                    if (draggedWidget->isAncestorOf(targetContainer)) {
+                        return true;
+                    }
+
+                    // Remove the dragged element from containerInformation
+                    BuilderContainer* sourceContainer = dynamic_cast<BuilderContainer*>(parent());
+
+                    if (sourceContainer) {
+                        sourceContainer->removeElementFromContainerInformation(element);
+                    }
+                    
                 }
             }
 
             return true;
         }
     }
-
+  
     return QObject::eventFilter(watched, event);
 }
