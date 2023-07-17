c88cb70362c53163dd638679aac57a1dc8d33a92
Yash <yash001@admiralinstruments.com>
2023-07-16 23:15:57 -0700

    read the style from css

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,12 +1,30 @@
 #include "MainWindow.h"
 #include "MainWindowUI.h"
+#include <qfile.h>
+#include <qdebug.h>
 
 MainAppWindow::MainAppWindow(QWidget* parent)
     : QMainWindow(parent)
     , mainUI(std::make_unique<MainWindowUI>(this))
 {
+    applyStyleSheet();
     showMaximized();
 }
+
+void MainAppWindow::applyStyleSheet()
+{
+    
+    QFile styleFile(":stylesheet.css");
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QString qssContent = styleFile.readAll();
+        setStyleSheet(qssContent);
+        styleFile.close();
+    } else {
+        // Handle file loading error
+        qDebug() << "Failed to load stylesheet.qss resource file:" << styleFile.errorString();
+    }
+}
+
 MainAppWindow::~MainAppWindow()
 {
 }
