b67ca0453761f2cfb98616f304a92922087d246d
Yash <yash001@admiralinstruments.com>
2023-08-22 23:01:02 -0700

    add style for search bar.

--- src/SearchBarWidget.cpp
+++ src/SearchBarWidget.cpp
@@ -6,22 +6,27 @@ SearchBarWidget::SearchBarWidget(QWidget* parent)
     // Create a layout for the search bar
     searchBarLayout = new QHBoxLayout(this);
     searchBarLayout->setContentsMargins(0, 0, 0, 0);
-    searchBarLayout->setSpacing(5);
+    searchBarLayout->setSpacing(0);
 
     // Create the search icon label
     searchIconLabel = new QLabel(this);
-    QPixmap searchIconPixmap(":/resource/search.png"); // Replace with the path to your search icon
-    searchIconLabel->setPixmap(searchIconPixmap.scaled(16, 16)); // Adjust the size as needed
+    searchIconLabel->setObjectName("searchIcon");
+    QPixmap searchIconPixmap(":/resource/search_glass_icon.png"); // Replace with the path to your search icon
+    searchIconLabel->setPixmap(searchIconPixmap);
 
     // Create the line edit for entering the search text
     searchLineEdit = new QLineEdit(this);
+    searchLineEdit->setObjectName("searchLineEdit");
     searchLineEdit->setPlaceholderText("Search");
-    connect(searchLineEdit, &QLineEdit::textChanged, this, &SearchBarWidget::searchTextChanged);
+    
 
 
     // Create the search clear button
     searchClearButton = new QPushButton(this);
-
+    searchClearButton->setObjectName("searchClearButton");
+    searchClearButton->setIcon(QIcon(":/resource/search_clear_button.png"));
+    searchClearButton->hide();
+    
     // Add the search icon and line edit to the layout
     searchBarLayout->addWidget(searchIconLabel);
     searchBarLayout->addWidget(searchLineEdit);
@@ -30,4 +35,17 @@ SearchBarWidget::SearchBarWidget(QWidget* parent)
     // Set the layout as the main layout for this widget
     setLayout(searchBarLayout);
     setObjectName("searchBarWidget");
+
+    QObject::connect(searchClearButton, &QPushButton::clicked, this, [=]() {
+        searchLineEdit->clear();
+    });
+
+    connect(searchLineEdit, &QLineEdit::textChanged, this, [=](const QString& text) {
+        if (text.isEmpty()) {
+            searchClearButton->hide();
+        } else {
+            searchClearButton->show();
+        }
+        emit searchTextChanged(text);
+    });
 }
