d4761f5b110b3bdbcd8ddbcfa7629e709421833a
yash <yash001@admiralinstruments.com>
2024-10-31 10:13:35 -0700

    once the test is completed the exit the Test GUI.

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,5 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	app.setAttribute(Qt::AA_Use96Dpi, true);
-
-	TestManager::getInstance().runTests(argc, argv);
-    return app.exec();
+    return TestManager::getInstance().runTests(argc, argv);
 }
